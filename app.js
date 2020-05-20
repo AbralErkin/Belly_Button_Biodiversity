5945f9613f6409e8fa13a9263a18fcab9e378a17
Abral Erkin <abral@Abrals-MacBook-Pro.local>
2020-05-19 21:52:27 -0500

    application script

--- app.js
+++ app.js
@@ -1,78 +1,83 @@
 // Belly Button Biodiversity - Plotly.js
 
-function sampleMetadata(sample) {
 
-      d3.json("./samples.json").then(function(data){
+function sampleMetadata(met_data) {
+
+      d3.json("./samples.json").then(data => {
           var metaData = data.metadata;
     
           var demoInfo = d3.select("#sample-metadata");
           // Use `.html("") to Clear any Existing Metadata
           demoInfo.html("");
 
-          var metadataSelected = metaData.filter(meta => meta.id.toString() === sample)[0];
+          var metadataSelected = metaData.filter(data_filt => data_filt.id.toString() === met_data)[0];
           Object.entries(metadataSelected).forEach(([key, value]) => {
             demoInfo.append("h6").text(`${key}:${value}`);
           })
       })
-  } s
+  } 
   
-  // function plotCharts() {
+  function plotCharts(sample_data) {
   
-  //   // @TODO: Use `d3.json` to Fetch the Sample Data for the Plots
-  //   d3.json(`samples.json`).then((data) => {
-  //     // @TODO: Build a Bubble Chart Using the Sample Data
-  //     const otu_ids = data.otu_ids;
-  //     const otu_labels = data.otu_labels;
-  //     const sample_values = data.sample_values;
-  //     // @TODO: Build a Pie Chart
-  //     let bubbleLayout = {
-  //       margin: { t: 0 },
-  //       hovermode: "closests",
-  //       xaxis: { title: "OTU ID"}
-  //     }
+    d3.json("./samples.json").then(data => {
+
+      var selected_sample = data.samples.filter(data_filt => data_filt.id.toString() === sample_data)[0];
+      const otu_ids = selected_sample.otu_ids;
+      const otu_labels = selected_sample.otu_labels;
+      const sample_values = selected_sample.sample_values;
+
+      //bubble Chart
+      var bubbleLayout = {
+        margin: { t: 0 },
+        hovermode: "closests",
+        xaxis: { title: "OTU ID"}
+      }
   
-  //     let bubbleData = [
-  //       {
-  //         x: otu_ids,
-  //         y: sample_values,
-  //         text: otu_labels,
-  //         mode: "markers",
-  //         marker: {
-  //           size: sample_values,
-  //           color: otu_ids,
-  //           colorscale: "Earth"
-  //         }
-  //       }
-  //     ]
+      var bubbleData = [
+        {
+          x: otu_ids,
+          y: sample_values,
+          text: otu_labels,
+          mode: "markers",
+          marker: {
+            size: sample_values,
+            color: otu_ids,
+            colorscale: "Earth"
+          }
+        }
+      ]
   
-  //     Plotly.plot("bubble", bubbleData, bubbleLayout);
-  
-  //     // HINT: Use slice() to Grab the Top 10 sample_values,
-  //     // otu_ids, and otu_labels (10 Each)
-  //     let pieData = [
-  //       {
-  //         values: sample_values.slice(0, 10),
-  //         labels: otu_ids.slice(0, 10),
-  //         hovertext: otu_labels.slice(0, 10),
-  //         hoverinfo: "hovertext",
-  //         type: "pie"
-  //       }
-  //     ];
+      Plotly.newPlot("bubble", bubbleData, bubbleLayout);
       
-  //     let pieLayout = {
-  //       margin: { t: 0, l: 0 }
-  //     };
+      //bar chart
+      var otu_temp = otu_ids.slice(0, 10);
+      var OTU_id = otu_temp.map(d => "OTU " + d);
+      var barData =[
+        {
+          x: sample_values.slice(0, 10),
+          y: OTU_id,
+          text: otu_labels.slice(0, 10),
+          orientation: "h",
+          type: "bar",
+        }];
+      
+      var barLayout = {
+        title: "Top 10 OTU",  
+          yaxis:{
+              autorange: "reversed"
+            }
+      };
   
-  //     Plotly.plot("pie", pieData, pieLayout)
-  // })
-  // }
+      Plotly.newPlot("bar", barData, barLayout)
+  })
+  }
   
   function init() {
 
     var dropDown = d3.select("#selDataset");
   
     // Use the List of Sample Names to Populate the Select Options
-    d3.json("samples.json").then((data) => {
+    d3.json("./samples.json").then(data => {
       sampleNames = data.names;
       sampleNames.forEach((name) => {
         dropDown
@@ -83,14 +88,14 @@ function sampleMetadata(sample) {
   
       // Use the First Sample from the List to Build Initial Plots
       const firstSample = sampleNames[0];
-     // plotCharts(firstSample);
+      plotCharts(firstSample);
       sampleMetadata(firstSample);
-    });
+    })
   }
   
   function optionChanged(newSample) {
     // Fetch New Data Each Time a New Sample is Selected
-    //plotCharts(newSample);
+    plotCharts(newSample);
     sampleMetadata(newSample);
   }
   
